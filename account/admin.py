f3d5dda7352b5f683d443e24109b36953fcc8a9a
vahid <vahidrajabi.pro7913@gmail.com>
2023-04-27 15:07:36 +0430

    Create CustomUser

---
+++ account/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+@admin.register(get_user_model())
+class CustomUserAdmin(UserAdmin):
+    model = get_user_model()
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = ['username', 'email', 'is_staff']
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Local Apps
+    'account.apps.AccountConfig',
+    
+    
 ]
 
 MIDDLEWARE = [
@@ -127,3 +132,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Accounts Config
+AUTH_USER_MODEL = 'account.CustomUser'
